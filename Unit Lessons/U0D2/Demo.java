5a06359e045caa718292e956b81641a6f2e841c1
Ang-In <crystal.mailo4o3@gmail.com>
2025-08-29 15:02:14 -0700

    Day 2 Complete
    Completed Lesson U0D2, class attribute introduction & method formation
    Began Unit 0 Exercises, complete by Tues 09-02-2025

---
+++ Unit Lessons/U0D2/Demo.java
@@ -0,0 +1,15 @@
+// class -> set of attributes & methods
+public class Demo {
+    public static void main(String[] args) {
+        Demo sample = new Demo();
+        System.out.println(sample.havingFun());
+    }
+
+    public String havingFun() {
+        return "I'm having fun!";
+    }
+
+    public void hello() {
+
+    }
+}
---
+++ Unit Lessons/U0D2/Zombie.java
@@ -0,0 +1,42 @@
+public class Zombie {
+    // instance variables (attributes)
+
+    // green
+    String skinColor = "green";
+
+    // rot and dead
+    boolean isDead = true;
+
+    // humans eaten count
+    int humansEaten = 0;
+
+    // main
+
+    public static void main(String[] args) {
+        Zombie phillip = new Zombie();
+
+        phillip.growl();
+        System.out.println(phillip.growl());
+        System.out.println("Phillip ate " + phillip.getHumansEaten() + " humans.");
+
+        Zombie brian = new Zombie();
+
+        brian.growl();
+        System.out.println(brian.growl());
+
+
+    }
+
+    // methods (behaviors)
+
+    // growl
+    public String growl() {
+        return "grrrsssaaaaajlhhhhh...";
+    }
+
+    // Challenge: make a new method that returns the number of humans eaten
+
+    public int getHumansEaten() {
+        return humansEaten;
+    }
+}
