875f92e37ed792369a53068c207f562062a57154
Ang-In <crystal.mailo4o3@gmail.com>
2025-09-17 14:08:49 -0700

    Unit 0 Day 8 Worksheet Completed
    Finished U0D8 Worksheet, began Student Profiles Lab

---
+++ Assignments/Unit 0 Student Profiles Lab/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    // instance variables
+
+    private String name, id;
+    private int grade;
+
+    // constructors
+
+    public Student(String name, int grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Student(String name){
+        this.name = name;
+        grade = 10;
+    }
+}
