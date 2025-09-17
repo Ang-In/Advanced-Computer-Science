1037b98af16c52463f1ce5b16eb4ae34c73039c4
Ang-In <crystal.mailo4o3@gmail.com>
2025-09-17 14:49:41 -0700

    Day 8 Completed
    Completed Student Profiles Lab

--- Assignments/Unit 0 Student Profiles Lab/Student.java
+++ Assignments/Unit 0 Student Profiles Lab/Student.java
@@ -1,18 +1,100 @@
 public class Student {
     // instance variables
 
-    private String name, id;
+    private String name, id = "";
     private int grade;
+    private Student latestOpp, recentFriend;
 
     // constructors
 
-    public Student(String name, int grade){
+    public Student(String name, int grade) {
         this.name = name;
+        this.grade = grade;
+        this.latestOpp = null;
+    }
+
+    public Student(String name) {
+        this.name = name;
+        grade = 10;
+        latestOpp = null;
+    }
+
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Student getLatestOpp() {
+        return latestOpp;
+    }
+
+    public Student getRecentFriend() {
+        return recentFriend;
+    }
+
+    // setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(int grade) {
         this.grade = grade;
     }
 
-    public Student(String name){
-        this.name = name;
-        grade = 10;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLatestOpp(Student latestOpp) {
+        this.latestOpp = latestOpp;
+    }
+
+    public void setRecentFriend(Student recentFriend) {
+        this.recentFriend = recentFriend;
+    }
+
+    // methods
+
+    public String vibeCheck(Student other) {
+        if (this.latestOpp.equals(other.latestOpp)) {
+            recentFriend = other;
+            return this.name + " and " + other.name + " are now friends! They both dislike "
+                    + this.latestOpp.name + ".";
+        }
+
+        return this.name + " and " + other.name
+                + " are not friends, they dislike different people.";
+    }
+
+    public String generateId() {
+        for (int i = 0; i < 9; i++) {
+            if (id.length() == 3) {
+                id += "-";
+            } else {
+                id += (int) (Math.random() * 10);
+            }
+        }
+
+        return id;
+    }
+
+    public boolean equals(Student other) {
+        return this.name.equals(other.name) && this.grade == other.grade
+                && this.id.equals(other.id);
+    }
+
+    public String toString() {
+        return name + " is a " + grade + "th grade student. Their id is " + id + ".";
+        // , who likes " + recentFriend.name + " and dislikes " + latestOpp.name + "
     }
 }
--- Assignments/Unit 0 Student Profiles Lab/StudentTester.java
+++ Assignments/Unit 0 Student Profiles Lab/StudentTester.java
@@ -1,3 +1,53 @@
 public class StudentTester {
-    
+    public static void main(String[] args) {
+        // test constructors
+        Student silly = new Student("Cornelia", 9);
+        Student annoying = new Student("Tyler", 11);
+        Student quiet = new Student("N");
+        Student dreamer = new Student("Wish");
+
+        // test toString()
+        System.out.println(silly.toString());
+        System.out.println(annoying.toString());
+        System.out.println(quiet.toString());
+        System.out.println(dreamer.toString());
+
+        // test getters
+        silly.generateId();
+        annoying.generateId();
+        quiet.generateId();
+        dreamer.generateId();
+
+        System.out.println(silly.toString());
+        System.out.println(annoying.toString());
+        System.out.println(quiet.toString());
+        System.out.println(dreamer.toString());
+
+        // test setters
+        silly.setGrade(10);
+        annoying.setGrade(12);
+        quiet.setGrade(11);
+        dreamer.setGrade(11);
+
+        System.out.println(silly.toString());
+        System.out.println(annoying.toString());
+        System.out.println(quiet.toString());
+        System.out.println(dreamer.toString());
+
+        // test equals()
+        System.out.println("Are " + quiet.getName() + " and " + dreamer.getName() + " equal? "
+                + quiet.equals(dreamer));
+
+        // Stretch Challenge: test vibeCheck
+        silly.setLatestOpp(annoying);
+        dreamer.setLatestOpp(annoying);
+
+        System.out.println(silly.toString());
+        System.out.println(dreamer.toString());
+
+        System.out.println(dreamer.vibeCheck(silly));
+        System.out.println(
+                dreamer.getName() + "'s newest friend: " + dreamer.getRecentFriend().getName());
+
+    }
 }
