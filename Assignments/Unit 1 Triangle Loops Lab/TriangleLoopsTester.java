f85b0059a8bb189858a72db89b438155ace2e91d
Ang-In <crystal.mailo4o3@gmail.com>
2025-10-22 13:33:31 -0700

    Day 17 Addendum
    Added comments to TriangleLoopsTester

--- Assignments/Unit 1 Triangle Loops Lab/TriangleLoopsTester.java
+++ Assignments/Unit 1 Triangle Loops Lab/TriangleLoopsTester.java
@@ -4,21 +4,25 @@ public class TriangleLoopsTester {
         // to-do: invoke each method at least 2 times in the tester class
 
         // test createLetterTriangleUp():
+        System.out.println("== Testing createLetterTriangleUp() ==");
         System.out.println(TriangleLoops.createLetterTriangleUp(3, 'A'));
         System.out.println(TriangleLoops.createLetterTriangleUp(15, 'B'));
         System.out.println(TriangleLoops.createLetterTriangleUp(-5, 'B')); // edge case
 
         // test createLetterTriangleDown():
+        System.out.println("== Testing createLetterTriangleUp() ==");
         System.out.println(TriangleLoops.createLetterTriangleDown(3, 'A'));
         System.out.println(TriangleLoops.createLetterTriangleDown(15, 'B'));
         System.out.println(TriangleLoops.createLetterTriangleDown(-5, 'B')); // edge case
 
         // test createNumbersTriangle():
+        System.out.println("== Testing createLetterTriangleUp() ==");
         System.out.println(TriangleLoops.createNumbersTriangle(3));
         System.out.println(TriangleLoops.createNumbersTriangle(15));
         System.out.println(TriangleLoops.createNumbersTriangle(-5)); // edge case
 
         // test createAlphabetTriangle():
+        System.out.println("== Testing createLetterTriangleUp() ==");
         System.out.println(TriangleLoops.createAlphabetTriangle(3));
         System.out.println(TriangleLoops.createAlphabetTriangle(15));
         System.out.println(TriangleLoops.createAlphabetTriangle(-5)); // edge case
