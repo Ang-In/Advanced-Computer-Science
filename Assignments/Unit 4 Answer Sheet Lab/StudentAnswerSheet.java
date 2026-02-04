3c2c26bcfbf8bf893f5314e940e98af389cbda0a
Ang-In <crystal.mailo4o3@gmail.com>
2026-02-04 09:24:28 -0800

    Day 42 Complete
    The answer to life, the universe, and everything.
    Began Lab 4.7 Answer Sheet (Due 2/12/2026)

---
+++ Assignments/Unit 4 Answer Sheet Lab/StudentAnswerSheet.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class StudentAnswerSheet {
+    // instance variables
+    String name;
+    double testScore;
+    ArrayList<String> answers;
+
+    // constructors
+    
+}
